5e9f4e7546353d39b3ede98fd36ef2e5d364b7a8
jonathafernandes <jonathafernandes@live.com>
2023-02-24 15:32:24 -0300

    arquivos de projetos para pratica

---
+++ manipulacao-strings/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //para uma variável que inicia em 1, e vai até 10, mudando 1-por-1, faça:
+        //tabuada de multiplicação:
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(j + "x" + i + " = " + j * i);
+            }
+        }
+
+    }
+}
